425f80bd0c909e26cbdc327a9d2e86012b0b6d7f
DoubleZ <kohei.tashiro@chiba-u.jp>
2020-03-28 15:40:50 +0900

    カロリーの読み取り機能を追加

--- ringfitter.py
+++ ringfitter.py
@@ -13,6 +13,8 @@ import datetime
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './client_credentials.json'
 THRESHOLD=0.95
+TEMPLATE = cv2.imread('template.jpg')
+
 
 def readText(image):
     # Instantiates a client
@@ -42,11 +44,17 @@ def readTime(time_image):
     if(len(time_array)==3):
         return datetime.time(hour=int(time_array[0]),minute=int(time_array[1]),second=int(time_array[2]))
 
+def readCal(cal_image):
+    texts = readText(cal_image)
+    for text in texts:
+        print(text.description)
+    # 小数付きでkcalを返す
+    return float(re.findall("\d+\.\d+",texts[0].description)[0])
+
 def isResultImage():
-    template = cv2.imread('template.jpg')
     fetch_image = cv2.imread('temp.jpg')
 
-    result = cv2.matchTemplate(fetch_image, template, cv2.TM_CCOEFF_NORMED)
+    result = cv2.matchTemplate(fetch_image, TEMPLATE, cv2.TM_CCOEFF_NORMED)
     #検出結果から検出領域の位置を取得
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
     print(max_val)
@@ -54,7 +62,7 @@ def isResultImage():
     else: return False
 
 def ImageToText():
-    fetch_image = cv2.imread('template.jpg')
+    fetch_image = cv2.imread('temp.jpg')
 
     #各部分の画像を切り出す
     total_time =  fetch_image[257:257+84,552:552+404]
@@ -63,5 +71,3 @@ def ImageToText():
 
     time = readTime(total_time)
     return (str(time)+' 頑張ったね！お疲れ様！')
-
-isResultImage()
