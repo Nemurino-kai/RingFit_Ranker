8112111f248efd3caa416a1c38733a584a730d5a
DoubleZ <kohei.tashiro@chiba-u.jp>
2020-05-19 21:48:00 +0900

    ページの追加

--- info_pages.py
+++ info_pages.py
@@ -14,12 +14,16 @@ bootstrap = Bootstrap(app)
 def index():
     conn = sqlite3.connect(config.DATABASE_NAME)
     cur = conn.cursor()
-    cur.execute("select user_name,kcal,tweeted_time from Exercise "
+    cur.execute("select RANK() OVER(ORDER BY kcal DESC) AS ranking,user_name,kcal,tweeted_time from Exercise "
                 "WHERE date(time_stamp) == date('now','+9 hour') ORDER BY kcal DESC ;"
     )
     exercise_data_list = cur.fetchall()
     print(exercise_data_list)
     return render_template('index.html',results = exercise_data_list)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
