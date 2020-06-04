ffd6f1b378f7145ce6156c99204004e264a6587b
DoubleZ <kohei.tashiro@chiba-u.jp>
2020-06-05 00:09:24 +0900

    ランキング集計に指定できる日付を制限

--- info_pages.py
+++ info_pages.py
@@ -20,8 +20,7 @@ def index():
     conn = sqlite3.connect(config.DATABASE_NAME)
     cur = conn.cursor()
 
-    if day == None:
-        if datetime.datetime.now(JST).hour < 4:
+    if datetime.datetime.now(JST).hour < 4:
             # 昨日の4時～今
             now = datetime.datetime.now(JST)
             yesterday = now - datetime.timedelta(days=1)
@@ -29,14 +28,17 @@ def index():
             start_day = yesterday.strftime("%Y-%m-%d")
             start_t = start_day + " 04:00:00"
             params = (start_t,stop_t)
-        else:
+            max_day=start_day
+    else:
             # 今日の4時～今
             now = datetime.datetime.now(JST)
             stop_t = now.strftime("%Y-%m-%d %H:%M:%S")
             start_day = now.strftime("%Y-%m-%d")
             start_t = start_day + " 04:00:00"
             params = (start_t,stop_t)
-    else:
+            max_day = start_day
+
+    if day is not None:
         # 指定日前日の4時～指定日の3時59分59秒
         start_day = day
         stop_day = datetime.datetime.strftime(datetime.datetime.strptime(day,'%Y-%m-%d') + datetime.timedelta(days=1),'%Y-%m-%d')
@@ -52,7 +54,7 @@ def index():
 
     exercise_data_list = cur.fetchall()
     print(exercise_data_list)
-    return render_template('index.html',results = exercise_data_list,start_t = start_t,start_day=start_day,stop_t=stop_t)
+    return render_template('index.html',results = exercise_data_list,start_t = start_t,start_day=start_day,stop_t=stop_t,max_day=max_day)
 
 @app.route('/about')
 def about():
