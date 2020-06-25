395e8dffafb89dacd93a9b9404b61043e8c3a7ed
DoubleZ <kohei.tashiro@chiba-u.jp>
2020-06-25 16:40:20 +0900

    Userごとの消費カロリー履歴を見れるページの追加

--- info_pages.py
+++ info_pages.py
@@ -20,36 +20,20 @@ def index():
     conn = sqlite3.connect(config.DATABASE_NAME)
     cur = conn.cursor()
 
-    if datetime.datetime.now(JST).hour < 4:
-            # 昨日の4時～今
-            now = datetime.datetime.now(JST)
-            yesterday = now - datetime.timedelta(days=1)
-            stop_t = now.strftime("%Y-%m-%d %H:%M:%S")
-            start_day = yesterday.strftime("%Y-%m-%d")
-            start_t = start_day + " 04:00:00"
-            params = (start_t,stop_t)
-            max_day=start_day
-    else:
-            # 今日の4時～今
-            now = datetime.datetime.now(JST)
-            stop_t = now.strftime("%Y-%m-%d %H:%M:%S")
-            start_day = now.strftime("%Y-%m-%d")
-            start_t = start_day + " 04:00:00"
-            params = (start_t,stop_t)
-            max_day = start_day
+    now = datetime.datetime.now(JST)
+    ranking_timestamp = now - datetime.timedelta(hours=4)
+    max_day = ranking_timestamp.strftime("%Y-%m-%d")
 
-    if day is not None:
-        # 指定日前日の4時～指定日の3時59分59秒
-        start_day = day
-        stop_day = datetime.datetime.strftime(datetime.datetime.strptime(day,'%Y-%m-%d') + datetime.timedelta(days=1),'%Y-%m-%d')
-        stop_t = stop_day + " 03:59:59"
-        start_t = start_day + " 04:00:00"
-        params = (start_t, stop_t)
+    if day is None:
+        day = max_day
+
+    stop_day = (datetime.datetime.strptime(day,"%Y-%m-%d")+datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    params=(day,)
 
     print(params)
 
     cur.execute("SELECT RANK() OVER(ORDER BY kcal DESC) AS ranking,user_name,kcal,tweeted_time "
-                "FROM (SELECT *, RANK() OVER(PARTITION BY user_screen_name ORDER BY kcal DESC, id) AS rnk FROM Exercise WHERE  time_stamp BETWEEN ? AND ?) tmp "
+                "FROM (SELECT *, RANK() OVER(PARTITION BY user_screen_name ORDER BY kcal DESC, id) AS rnk FROM Exercise WHERE   date(datetime(time_stamp,'-4 hours'))==?) tmp "
                 "WHERE rnk = 1 ORDER BY kcal DESC, tweeted_time ASC;",params)
 
     exercise_data_list = cur.fetchall()
@@ -58,13 +42,34 @@ def index():
     res = exercise_data_list[(page - 1) * 100: page * 100]
     pagination = Pagination(page=page, total=len(exercise_data_list), per_page=100, css_framework='bootstrap4')
 
-    return render_template('index.html',results = res,start_t = start_t,start_day=start_day,
-                           stop_t=stop_t,max_day=max_day, pagination=pagination)
+    return render_template('index.html',results = res,start_day=day,
+                           stop_day=stop_day,max_day=max_day, pagination=pagination)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+@app.route('/user')
+def user():
+    user = request.args.get('user')
+    conn = sqlite3.connect(config.DATABASE_NAME)
+    cur = conn.cursor()
+    if user is None: user = ""
+    params=(user,)
+
+    cur.execute("SELECT kcal,tweeted_time "
+                "FROM (SELECT *, RANK() OVER(PARTITION BY user_screen_name, date(datetime(time_stamp,'-4 hours')) ORDER BY kcal DESC, id) AS rnk FROM Exercise WHERE user_screen_name==?) tmp "
+                "WHERE rnk = 1 ORDER BY tweeted_time DESC;",params)
+
+    exercise_data_list = cur.fetchall()
+
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    res = exercise_data_list[(page - 1) * 100: page * 100]
+    pagination = Pagination(page=page, total=len(exercise_data_list), per_page=100, css_framework='bootstrap4')
+
+    return render_template('user.html',results =res,pagination=pagination,user=user)
+
+# TODO:未完成のページ
 @app.route('/analytics')
 def analytics():
     day = request.args.get('day')
