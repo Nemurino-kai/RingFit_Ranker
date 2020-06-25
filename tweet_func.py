46fd362cbf824489f00104a94f105ee747766aff
DoubleZ <kohei.tashiro@chiba-u.jp>
2020-06-25 16:40:36 +0900

    SQL文のリファクタリング

--- tweet_func.py
+++ tweet_func.py
@@ -74,25 +74,13 @@ def reply_ranking(api,item_num=100):
         conn = sqlite3.connect(config.DATABASE_NAME)
         cur = conn.cursor()
 
-        if datetime.datetime.now(JST).hour < 4:
-            # 昨日の4時～今
-            now = datetime.datetime.now(JST)
-            yesterday = now - datetime.timedelta(days=1)
-            stop_t = now.strftime("%Y-%m-%d %H:%M:%S")
-            start_day = yesterday.strftime("%Y-%m-%d")
-            start_t = start_day + " 04:00:00"
-            max_day = start_day
-        else:
-            # 今日の4時～今
-            now = datetime.datetime.now(JST)
-            stop_t = now.strftime("%Y-%m-%d %H:%M:%S")
-            start_day = now.strftime("%Y-%m-%d")
-            start_t = start_day + " 04:00:00"
-            max_day = start_day
-        params = (tweet.user.id,start_t, stop_t)
+        now = datetime.datetime.now(JST)
+        ranking_timestamp = now - datetime.timedelta(hours=4)
+
+        params = (tweet.user.id,ranking_timestamp)
 
         # user_idからカロリーを抽出
-        cur.execute("SELECT kcal FROM Exercise WHERE user_id == ? AND time_stamp BETWEEN ? AND ?",params)
+        cur.execute("SELECT kcal FROM Exercise WHERE user_id == ? AND date(datetime(time_stamp,'-4 hours'))==?",params)
 
 
 def make_ranking_picture(exercise_data_list):
