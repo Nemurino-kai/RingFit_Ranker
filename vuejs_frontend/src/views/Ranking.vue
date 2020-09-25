9db53f56e9d78afd3a49c33197e297964d8b365f
DoubleZ <kohei.tashiro@chiba-u.jp>
2020-09-25 23:37:23 +0900

    集計日を参照して、APIにアクセス、ランキングを表示するようにした

--- vuejs_frontend/src/views/Ranking.vue
+++ vuejs_frontend/src/views/Ranking.vue
@@ -2,13 +2,12 @@
   <div class="ranking container">
     <h2 >Exercise Ranking</h2> <div class="text-muted">({{exercise_data.start_day}} 04:00:00 ～ {{exercise_data.stop_day}} 03:59:59)</div>
     <hr style="margin-top:0.5em;margin-bottom:1em;">
-                <form class="form-group form-inline">
-                        <label>集計日&nbsp;</label>
-                        <input type="text" placeholder="Select Date.." name="day">
-                        &nbsp;<button type="submit"  class="btn btn-primary">集計する</button>
-                </form>
+    <form class="form-group form-inline">
+      <label>集計日&nbsp;</label>
+      <Datepicker v-model="defaultDate" :disabled-dates="disabledDates"></Datepicker>
+    </form>
 
-  <table class="table table-bordered">
+  <table class="table table-bordered" v-if="exercise_data.length != 0">
     <thead>
       <tr><th>Rank</th><th>Username</th><th>kcal</th><th>Date</th></tr>
     </thead>
@@ -24,16 +23,39 @@
 </template>
 
 <script>
-import axios from 'axios'
+
+function formatDate (date) {
+  var d = new Date(date)
+  var month = '' + (d.getMonth() + 1)
+  var day = '' + d.getDate()
+  var year = d.getFullYear()
+
+  if (month.length < 2) { month = '0' + month }
+  if (day.length < 2) { day = '0' + day }
+
+  return [year, month, day].join('-')
+}
 
 export default {
   data () {
     return {
+      defaultDate: new Date(),
+      disabledDates: {
+        to: new Date(2020, 5 - 1, 18),
+        from: new Date()
+      },
       exercise_data: []
     }
   },
-  mounted () {
-    axios.get('https://ringfit.work/api').then(res => {
+  watch: {
+    defaultDate: function (newDate) {
+      this.$api.get('https://ringfit.work/api?day=' + formatDate(newDate)).then(res => {
+        this.exercise_data = res['data']
+      })
+    }
+  },
+  created () {
+    this.$api.get('https://ringfit.work/api').then(res => {
       this.exercise_data = res['data']
     })
   }
